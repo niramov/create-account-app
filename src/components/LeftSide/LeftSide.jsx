3636b827a53242a892d9e64dd84f6fca052a10d4
Nikita Abramov <nikkita.abramov@gmail.com>
2024-02-10 00:15:58 +0100

    completed Leftside

--- src/components/LeftSide/LeftSide.jsx
+++ src/components/LeftSide/LeftSide.jsx
@@ -1,10 +1,16 @@
 import s from './LeftSide.module.scss';
+import mainPhoneImg from '../../assets/img/mainPhone.svg';
+import minorPhoneImg from '../../assets/img/smallPhone.svg';
 
 const LeftSide = () => (
   <div className={s.container}>
     <div className={s.heading}>Social media shared today, tomorrow or by location</div>
     <div className={s.ellipse}>
-      <div className={s.ellipseContainer}></div>
+      <div className={s.ellipseContainer}>
+        <img src={mainPhoneImg} alt="phone img" className={s.mainPhone} />
+        <img src={minorPhoneImg} alt="phone img" className={s.minorLeftPhone} />
+        <img src={minorPhoneImg} alt="phone img" className={s.minorRightPhone} />
+      </div>
     </div>
   </div>
 );
