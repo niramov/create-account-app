db71797671e4e55e6a56cbbb2e31348ab7287551
Nikita Abramov <nikkita.abramov@gmail.com>
2024-02-11 23:34:18 +0100

    add RightSide container

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,6 +1,8 @@
+import s from './RightSide.module.scss';
+
 const RightSide = () => (
-  <div>
-    <p>RightSide</p>
+  <div className={s.container}>
+    <div className={s.wrapper}></div>
   </div>
 );
 
