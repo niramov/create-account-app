34da9e1c14c766e80b7892af4ed2582abd678985
Nikita Abramov <nikkita.abramov@gmail.com>
2024-02-12 00:28:10 +0100

    add header & description

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,8 +1,17 @@
 import s from './RightSide.module.scss';
+import mainLogo from '../../assets/img/logo.svg';
 
 const RightSide = () => (
   <div className={s.container}>
-    <div className={s.wrapper}></div>
+    <div className={s.wrapper}>
+      <div className={s.logo}>
+        <img src={mainLogo} alt="logo" className={s.logo} />
+        <div className={s.formContainer}>
+          <h1 className={s.name}>Create account</h1>
+          <span className={s.description}>For business, band or celebrity.</span>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
