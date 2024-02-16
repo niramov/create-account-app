f8c4c2341e512559d33ddf77c43eaa80a4c6fda8
Nikita Abramov <nikkita.abramov@gmail.com>
2024-02-16 02:08:14 +0100

    fix вудуеу Navigate from App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import SuccessPage from './pages/SuccessPage/SuccessPage';
 import routes from './routes/routes';
